053529c2a887b49d6ae0a54885b7d142864030ab
CN\YANGSS5 <yangss5@meicloud.com>
2021-05-10 16:14:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <PieChart 
+    :height="300"
+    :width="400"
+    bordered
+    :series="{
+      data: [
+        { name: 'China', value: 49 },
+        { name: 'USA', value: 69 },
+        { name: 'Japan', value: 29 },
+        { name: 'Russia', value: 39 }
+      ]
+    }"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { PieChart } from '@yangss/vue-echarts'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    PieChart
+  }
+})
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
